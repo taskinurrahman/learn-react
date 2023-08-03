3d94c2ce0346251ef51c473f4ab470243dd9cd33
Taskinur Rahman <taskinur.rahman2014@gmail.com>
2023-08-03 16:08:39 +0600

    using react hook form

--- src/components/NewExpense.jsx
+++ src/components/NewExpense.jsx
@@ -1,53 +1,58 @@
 import './NewExpense.css'
 import {useState} from "react";
+import {useForm} from "react-hook-form";
 
 const NewExpense = (props) => {
 
-    const [title,setTitle] = useState('')
-    const [amount,setAmount] = useState('')
-    const [date,setDate] = useState('')
+    const {register,handleSubmit} = useForm();
 
-    const titleChangeHandler = (event)=>{
-        setTitle(()=>event.target.value)
-    }
-    const amountChangeHandler = (event)=>{
-        setAmount(()=>event.target.value)
-    }
-    const dateChangeHandler = (event)=>{
-        setDate(()=>event.target.value)
-    }
+    // const [title,setTitle] = useState('')
+    // const [amount,setAmount] = useState('')
+    // const [date,setDate] = useState('')
+    //
+    // const titleChangeHandler = (event)=>{
+    //     setTitle(()=>event.target.value)
+    // }
+    // const amountChangeHandler = (event)=>{
+    //     setAmount(()=>event.target.value)
+    // }
+    // const dateChangeHandler = (event)=>{
+    //     setDate(()=>event.target.value)
+    // }
 
-    function formatSubmitHandler(event) {
-        event.preventDefault()
-
-        const expenseData = {
-            id:'f1',
-            title:title,
-            amount:amount,
-            date: new Date(date),
-        };
-        props.onSaveExpense(expenseData)
-
-        setTitle('')
-        setAmount('')
-        setDate('')
-    }
+    // function formSubmitHandler(event) {
+    //     event.preventDefault()
+    //
+    //     const expenseData = {
+    //         id:'f1',
+    //         title:title,
+    //         amount:amount,
+    //         date: new Date(date),
+    //     };
+    //     props.onSaveExpense(expenseData)
+    //
+    //     setTitle('')
+    //     setAmount('')
+    //     setDate('')
+    // }
 
     return (
         <div className="new-expense">
-            <form onSubmit={formatSubmitHandler}>
+            <form onSubmit={handleSubmit((data)=>{
+                    props.onSaveExpense(data)
+            })}>
                 <div className='new-expense__controls'>
                     <div className='new-expense__control'>
                         <label>Title</label>
-                        <input type='text' value={title} onChange={titleChangeHandler}/>
+                        <input  {...register("title",{required:"This is required"})}/>
                     </div>
                     <div className='new-expense__control'>
                         <label>Amount</label>
-                        <input type='number' min='0.01' step='0.01' value={amount} onChange={amountChangeHandler}/>
+                        <input {...register("amount",{required:"This is required"})} type='number' min='0.01' step='0.01'/>
                     </div>
                     <div className='new-expense__control'>
                         <label>Date</label>
-                        <input type='date' value={date} min='2019-01-01' max='2023-12-01' onChange={dateChangeHandler}/>
+                        <input {...register("date",{required:"This is required"})} type='date' />
                     </div>
                 </div>
                 <div className='new-expense__actions'>
